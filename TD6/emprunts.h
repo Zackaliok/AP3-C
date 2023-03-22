ae7d574be4e0edb590de3c38f8ca3b1d36c13194
Tristan Fumiere <t.fumiere@actirise.com>
2023-03-22 20:39:59 +0100

    fix

--- TD6/emprunts.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by tristan on 15/03/23.
-//
-
-#ifndef AP3_C_EMPRUNTS_H
-#define AP3_C_EMPRUNTS_H
-
-#endif //AP3_C_EMPRUNTS_H
--- TD6/livres.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by tristan on 15/03/23.
-//
-
-#ifndef AP3_C_LIVRES_H
-#define AP3_C_LIVRES_H
-
-#endif //AP3_C_LIVRES_H
